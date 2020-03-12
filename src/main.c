07e93e855050e095996f58630072b16dc6c4ad3f
陈昱安 <31612534@qq.com>
2020-03-12 09:09:29 +0800

    测试成功
    Signed-off-by: 陈昱安 <31612534@qq.com>

--- src/main.c
+++ src/main.c
@@ -1,14 +1,37 @@
 #include "main.h"
-
+TaskHandle_t xHandle1 = NULL;
+TaskHandle_t xHandle2 = NULL;
 /*-----------------------------------------------------------*/
-void task1(void *para)
+void test(const char *str)
 {
     int flag = 0;
     while (1)
     {
         flag++;
-        printf("hello freeRTOS:%d\n", flag);
+        printf("[%u]ms %s:%d\n", xTaskGetTickCount(), str, flag);
         vTaskDelay(500);
+    }
+}
+void task1(void *para)
+{
+    test("i am task1");
+    vTaskDelete(NULL);
+}
+void task2(void *para)
+{
+    test("i am task2");
+    vTaskDelete(NULL);
+}
+void task3(void *para)
+{
+    while (1)
+    {
+        if (xTaskGetTickCount() >= 5000)
+        {
+            vTaskDelete(xHandle1);
+            xTaskCreate(task2, "task2", 512, NULL, 1, &xHandle2);
+            break;
+        }
     }
     vTaskDelete(NULL);
 }
@@ -16,7 +39,9 @@ int main(void)
 {
     prvInitialiseHeap();
     vTraceEnable(TRC_START);
-    xTaskCreate(task1, "task1", 512, NULL, 1, NULL);
+
+    xTaskCreate(task1, "task1", 512, NULL, 1, &xHandle1);
+    xTaskCreate(task3, "task3", 512, NULL, 1, NULL);
     vTaskStartScheduler();
     while (1)
         ;
